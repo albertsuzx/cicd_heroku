d47bb745d48d8a80a28691bbb5ae141c062b5694
Zhixue Su <zhixue.su@entercard.com>
2020-04-14 14:54:25 +0200

    add folder path so test file could import main

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,8 @@
+import sys, os
+testdir = os.path.dirname(__file__)
+srcdir = '../'
+sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
+
 from fastapi.testclient import TestClient
 
 from main import app
